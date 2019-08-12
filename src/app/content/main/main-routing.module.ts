298c46efdcc307abaf86c4a7dc6eab84b11ac908
Denis <den.belotesrkovets@gmail.com>
2019-08-12 11:31:57 +0300

    initial commit

---
+++ src/app/content/main/main-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent,
+        children: [
+            // {
+            //     path: '',
+            //     data: { state: 'list' },
+            //     loadChildren: './content/people-list/people-list.module#PeopleListModule',
+            // },
+            // {
+            //     path: ':id',
+            //     loadChildren: './content/details/details.module#DetailsModule',
+            // },
+        ],
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class MainRoutingModule {}
---
+++ src/app/content/main/main.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MainComponent } from './main.component';
+import { MainRoutingModule } from './main-routing.module';
+import { SharedModule } from '../../shared/shared.module';
+
+@NgModule({
+    declarations: [MainComponent],
+    imports: [MainRoutingModule, SharedModule],
+})
+export class MainModule {}
